a562f247d7fe551a5c82a735c2e25516ca5efd6c
Mark Frost <cheerful.mf@gmail.com>
2018-08-08 18:40:35 +0500

    refactoring

--- lib/cascade_touch.rb
+++ lib/cascade_touch.rb
@@ -5,37 +5,7 @@ require "cascade_touch/version"
 module CascadeTouch
   class << self
     def call(model_name, model_id, schema)
-      tree = Tree.new_from_hash(schema).children
-      updated = {}
-
-      start_model = model_name.constantize.find(model_id)
-      updater = Updater.new(start_model.updated_at)
-
-      tree.each do |children|
-        children.visit_all do |node|
-          if node.parent && updated[node.parent]
-            for_update = updated[node.parent]
-
-            if for_update.is_a? Array
-              result = for_update.flatten.map do |model|
-                current_model = model.send(node.name)
-                next if current_model.nil?
-
-                updater.update_model_or_collection(current_model)
-              end
-            else
-              updater.update_model_or_collection(for_update.send(node.name))
-            end
-          else
-            current_model = start_model.send(node.name)
-            next if current_model.nil?
-
-            result = updater.update_model_or_collection(current_model)
-          end
-
-          updated[node.name] = result
-        end
-      end
+      Updater.call(model_name, model_id, schema)
     end
   end
 end
--- lib/cascade_touch/updater.rb
+++ lib/cascade_touch/updater.rb
@@ -1,8 +1,48 @@
 module CascadeTouch
   class Updater
-    def initialize(date)
-      @date = date
+    def initialize(model_name, model_id, schema)
+      @updated = {}
+
+      @tree = Tree.new_from_hash(schema).children
+      @start_model = model_name.constantize.find(model_id)
+      @date = @start_model.updated_at
     end
+
+    class << self
+      def call(*attrs)
+        new(*attrs).call
+      end
+    end
+
+    def call
+      @tree.each do |children|
+        children.visit_all do |node|
+          if node.parent && @updated[node.parent]
+            for_update = @updated[node.parent]
+
+            if for_update.is_a? Array
+              result = for_update.flatten.map do |model|
+                current_model = model.send(node.name)
+                next if current_model.nil?
+
+                update_model_or_collection(current_model)
+              end
+            else
+              update_model_or_collection(for_update.send(node.name))
+            end
+          else
+            current_model = @start_model.send(node.name)
+            next if current_model.nil?
+
+            result = update_model_or_collection(current_model)
+          end
+
+          @updated[node.name] = result
+        end
+      end
+    end
+
+    private
 
     def update_model_or_collection(model_or_collection)
       if model_or_collection.is_a? ActiveRecord::Base
